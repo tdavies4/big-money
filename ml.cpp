4559386e7cf6412d76c634cded8ae2f83363d68d
ibrahim408 <ibrahim408@users.noreply.github.com>
2015-06-08 13:25:09 -0700

    Update ml.cpp

--- ml.cpp
+++ ml.cpp
@@ -1,1 +1,5 @@
-dsdds
+virtual bool insert(const ItemType & newData) = 0;
+	virtual bool remove(const ItemType & data) = 0;
+	virtual bool getEntry(const ItemType & anEntry, ItemType & returnedItem) const = 0;
+	
+
